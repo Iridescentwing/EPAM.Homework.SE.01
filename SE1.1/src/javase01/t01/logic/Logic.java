92e756c715019411f70b9a8b24a4625a8c26b661
Екатерина <ekaterinarogozhina@gmail.com>
2016-03-06 01:06:59 +0300

    Resolve Task 1.

---
+++ SE1.1/src/javase01/t01/logic/Logic.java
@@ -0,0 +1,14 @@
+package javase01.t01.logic;
+
+/**
+ * Created by Ekaterina on 06.03.16.
+ */
+public class Logic {
+
+        public String method(){
+            return "I am string in Logic.";
+        }
+
+    }
+
+
---
+++ SE1.1/src/javase01/t01/main/Main.java
@@ -0,0 +1,14 @@
+package javase01.t01.main;
+
+import javase01.t01.logic.Logic;
+
+/**
+ * Created by Ekaterina on 06.03.16.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Logic logic = new Logic();
+        System.out.println(logic.method());
+    }
+}
